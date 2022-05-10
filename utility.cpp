d5eeaa6de8fe09f58d0c5f55e9b2ea6803a816cb
Joey Marcinowski <66542880+Pixels128@users.noreply.github.com>
2022-05-10 12:12:52 -0400

    Update utility.cpp

--- utility.cpp
+++ utility.cpp
@@ -1,22 +1,24 @@
-#include "../include/utility.h"
 #include <stdlib.h>
 #include <math.h>
-// #include "vex.h"
 
 float PI = 3.14159265;
 
+// Considering the robots current angle, return how far left the robot would need to turn to reach the target angle.
 int leftAngle(int currentAngle, int targetAngle){
   int turnAngle = 360 - ((360 - currentAngle + targetAngle) % 360);
   if (turnAngle == 360){return 0;}
   return -turnAngle;
 }
 
+// Considering the robots current angle, return how far right the robot would need to turn to reach the target angle.
 int rightAngle(int currentAngle, int targetAngle){
   int turnAngle = (360 - currentAngle + targetAngle) % 360;
   if (turnAngle == 360){return 0;}
   return turnAngle;
 }
 
+// considering the left and right error, return how much the robot would need to turn for the lesser distance.
+// a negative value represents a left hand turn, while right is positive.
 int getTurnError(int currentAngle, int targetAngle)
 {
   if (abs(leftAngle(currentAngle, targetAngle)) < abs(rightAngle(currentAngle, targetAngle))){
@@ -24,53 +26,25 @@ int getTurnError(int currentAngle, int targetAngle)
   }
   else{return rightAngle(currentAngle, targetAngle);}
 }
-
+// return the square of a value.
 float square(float x)
 {
   return x * x;
 }
-
+// convert radians to degrees.
 float radianToDegree(float x)
 {
   return x * (180.0 / PI);
 }
+
+// convert degrees to radians.
 float degreeToRadian(float x)
 {
   return x * (PI / 180.0);
 }
-/////////////////////////////////////
-// Unit Interface: Strategy Pattern
-/////////////////////////////////////
-double UnitInterface::unitToMM(double amount){return 0;};
-double UnitInterface::MMToUnit(double amount){return 0;};
 
-double INCHES_::unitToMM(double amount){return amount * 25.4f;};
-double INCHES_::MMToUnit(double amount){return amount / 25.4f;};
-
-double MM_::unitToMM(double amount){return amount;};
-double MM_::MMToUnit(double amount){return amount;};
-
-double CM_::unitToMM(double amount){return amount * 10.0f;};
-double CM_::MMToUnit(double amount){return amount / 10.0f;};
-
-double FEET_::unitToMM(double amount){return amount * 305.0f;};
-double FEET_::MMToUnit(double amount){return amount / 305.0f;};
-
-double METER_::unitToMM(double amount){return amount * 1000.0f;};
-double METER_::MMToUnit(double amount){return amount / 1000.0f;};
-
-double MILES_::unitToMM(double amount){return amount * 1.609f * 10 * 10 * 10 * 10 * 10 * 10;};
-double MILES_::MMToUnit(double amount){return amount / 1.609f * 10 * 10 * 10 * 10 * 10 * 10;};
-
-double SMOOT_::unitToMM(double amount){return amount * 1701.8f;};
-double SMOOT_::MMToUnit(double amount){return amount / 1701.8f;};
-
-double convert(double amount, UnitInterface *firstType, UnitInterface *newType)
-{
-  double amountMM = firstType->unitToMM(amount);
-  return round(newType->MMToUnit(amountMM) * 10) / 10;
-}
-
+// considering the robots current X and Y position, calulate the heading the robot would need to drive at
+// to a new X, Y position.
 float getTargetAngle(float currentX, float targetX, float currentY, float targetY)
 {
   float targetAngle = atan((currentX - targetX) / (currentY - targetY));
@@ -84,6 +58,7 @@ float getTargetAngle(float currentX, float targetX, float currentY, float target
   return targetAngle -180;
 }
 
+// calulate the distance from a point
 float dist(int x1, int y1, int x2, int y2)
 {
     // Calculating distance
